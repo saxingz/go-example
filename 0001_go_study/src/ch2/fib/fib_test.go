4bf4281300c00fdb2d9d7ba9601445b0a6334df6
saxingz <saxlh@foxmail.com>
2020-07-19 20:28:30 +0800

    feat: go study

--- 0001_go_study/src/ch2/fib/fib_test.go
+++ 0001_go_study/src/ch2/fib/fib_test.go
@@ -14,9 +14,9 @@ func TestFibList(t *testing.T) {
 	//)
 	a := 1
 	b := 1
-	fmt.Print(a)
+	t.Log(a)
 	for i := 0; i < 5; i++ {
-		fmt.Print(" ", b)
+		t.Log(" ", b)
 		tmp := a
 		a = b
 		b = tmp + a
