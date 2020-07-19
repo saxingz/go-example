828021c766cd104fa390667f02c06dc3ff4ac39a
saxingz <saxlh@foxmail.com>
2020-07-19 20:25:37 +0800

    feat: go study

---
+++ 0001_go_study/src/ch2/fib/fib_test.go
@@ -0,0 +1,23 @@
+package fib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFibList(t *testing.T) {
+	//var a int = 1
+	//var b int = 1
+	var (
+		a int = 1
+		b int = 1
+	)
+	fmt.Print(a)
+	for i := 0; i < 5; i++ {
+		fmt.Print(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
+	}
+	fmt.Println()
+}
