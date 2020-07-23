16e7df053dc42a5e04f8428038d9030e0307de4a
saxingz <saxlh@foxmail.com>
2020-07-23 23:00:53 +0800

    feat: go study

---
+++ 0001_go_study/src/ch4/condition/contidion_test.go
@@ -0,0 +1,22 @@
+package condition
+
+import "testing"
+
+func TestIfMultiSec(t *testing.T) {
+	if a := 1 == 1; a {
+		t.Log("1 == 1")
+	}
+}
+
+func TestSwitchMultiCase(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0, 2:
+			t.Log("Even")
+		case 1, 3:
+			t.Log("Odd")
+		default:
+			t.Log("not in 0 - 3")
+		}
+	}
+}
