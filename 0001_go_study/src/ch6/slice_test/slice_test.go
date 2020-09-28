dc77ad9592c05e755d2b088a16f93312875b33de
saxingz <saxlh@foxmail.com>
2020-09-28 22:04:04 +0800

    feat: go study

--- 0001_go_study/src/ch6/slice_test/slice_test.go
+++ 0001_go_study/src/ch6/slice_test/slice_test.go
@@ -9,4 +9,11 @@ func TestSliceInit(t *testing.T) {
 	s0 = append(s0, 1)
 	t.Log(len(s0), cap(s0))
 
+	s1 := []int{1, 2, 3, 4}
+	t.Log(len(s1), cap(s1))
+
+	s2 := make([]int, 3, 5)
+	t.Log(len(s2), cap(s2))
+	t.Log(s2[0], s2[1], s2[2])
+
 }
