2cbcffdeaccc7d6473649b56220bd1e88be7ca1e
saxingz <saxlh@foxmail.com>
2020-09-28 22:45:15 +0800

    feat: go study

--- 0001_go_study/src/ch8/map/map_ext_test.go
+++ 0001_go_study/src/ch8/map/map_ext_test.go
@@ -18,3 +18,23 @@ func TestMapWithFunValue(t *testing.T) {
 	}
 	t.Log(m[1](2), m[2](2), m[3](2))
 }
+
+func TestMapForSet(t *testing.T) {
+	mySet := map[int]bool{}
+	mySet[1] = true
+	n := 3
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
+	mySet[3] = true
+	t.Log(len(mySet))
+	delete(mySet, 1)
+	n = 1
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
+}
