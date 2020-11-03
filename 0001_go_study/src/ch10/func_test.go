1ca00d6599a26a6fb3cadedc6082fbd147686c8b
saxingz <saxlh@foxmail.com>
2020-11-03 22:47:10 +0800

    feat: go study

--- 0001_go_study/src/ch10/func_test.go
+++ 0001_go_study/src/ch10/func_test.go
@@ -36,3 +36,19 @@ func slowFun(op int) int {
 	time.Sleep(time.Second * 1)
 	return op
 }
+
+/**
+可变参数
+*/
+func Sum(ops ...int) int {
+	ret := 0
+	for _, op := range ops {
+		ret += op
+	}
+	return ret
+}
+
+func TestVarParam(t *testing.T) {
+	t.Log(Sum(1, 2, 3, 4))
+	t.Log(Sum(1, 2, 3, 4, 5))
+}
