1c1a63d0db753f88c9e5c9b1b77192ae86285f1e
saxingz <saxlh@foxmail.com>
2020-11-13 23:14:38 +0800

    feat: go study

--- 0001_go_study/src/ch18/csp/async_service_test.go
+++ 0001_go_study/src/ch18/csp/async_service_test.go
@@ -21,3 +21,22 @@ func TestService(t *testing.T) {
 	fmt.Println(service())
 	otherTask()
 }
+
+func AsyncService() chan string {
+	//retCh := make(chan string)
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
+
+func TestAsynService(t *testing.T) {
+	retCh := AsyncService()
+	otherTask()
+	fmt.Println(<-retCh)
+	time.Sleep(time.Second * 1)
+}
