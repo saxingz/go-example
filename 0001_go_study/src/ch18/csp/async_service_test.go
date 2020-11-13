e42ed03d0cc9deed70814695d9a86a9ef1b548a1
saxingz <saxlh@foxmail.com>
2020-11-13 23:00:36 +0800

    feat: go study

---
+++ 0001_go_study/src/ch18/csp/async_service_test.go
@@ -0,0 +1,17 @@
+package csp
+
+import (
+	"fmt"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done")
+}
