f352178b8d3d00787b6780d7ca94f9836a1ad11a
saxingz <saxlh@foxmail.com>
2020-11-15 13:15:36 +0800

    feat: go study

--- 0001_go_study/src/ch33/obj_cache/sync_pool_test.go
+++ 0001_go_study/src/ch33/obj_cache/sync_pool_test.go
@@ -27,3 +27,27 @@ func TestSyncPool(t *testing.T) {
 	v2, _ := pool.Get().(int)
 	fmt.Println(v2)
 }
+
+func TestSyncPoolInMultiGroutine(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object")
+			return 10
+		},
+	}
+
+	pool.Put(100)
+	pool.Put(100)
+	pool.Put(100)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			//t.Log(pool.Get())
+			fmt.Println(pool.Get())
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
